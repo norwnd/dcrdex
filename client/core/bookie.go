6dc9e7bfdc49c4b1d1775be22bca6dabe2173a2e
norwnd <norwnd>
2023-03-09 05:58:36 +0300

    client/core: OrderBook clarify cache usage

--- client/core/bookie.go
+++ client/core/bookie.go
@@ -716,6 +716,9 @@ func handleTradeSuspensionMsg(c *Core, dc *dexConnection, msg *msgjson.Message) 
 		return fmt.Errorf("no order book found with market id '%s'", sp.MarketID)
 	}
 
+	// Since we don't subscribe to server feed here it's okay to use book.Reset,
+	// otherwise we'd have to use ResetBeforeSubscribe/ResetAfterSubscribe pair
+	// instead.
 	err = book.Reset(&msgjson.OrderBook{
 		MarketID: sp.MarketID,
 		Seq:      sp.Seq,        // forces seq reset, but should be in seq with previous
