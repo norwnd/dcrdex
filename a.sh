932b819de9a9fba2adec8c93c9e34e434dce1b03
norwnd <norwnd>
2024-12-24 12:30:01 +0200

    have pprof on by default

--- a.sh
+++ a.sh
@@ -2,12 +2,15 @@ set -e
 
 #!/usr/bin/env bash
 
-# mainnet
-(./client/cmd/bisonw/bisonw --webaddr=127.0.0.1:3333 --log=trace)
-# mainnet with dedicated DB:
+## mainnet
+# pprof is available at:
+# http://127.0.0.1:3333/debug/pprof/goroutine?debug=1
+(./client/cmd/bisonw/bisonw --webaddr=127.0.0.1:3333 --log=trace --httpprof)
+
+## mainnet with dedicated DB:
 #(./client/cmd/bisonw/bisonw --db=/Users/norwnd/d-e-x-c-db/db_mainnet --webaddr=127.0.0.1:3333 --log=trace)
-# testnet
+## testnet
 #(./client/cmd/bisonw/bisonw --db=/Users/norwnd/d-e-x-c-db/db_mainnet --webaddr=127.0.0.1:3333 --log=trace --testnet)
 
-# with disaster backups:
+## mainnet with disaster backups:
 #(./client/cmd/bisonw/bisonw --webaddr=127.0.0.1:3333 --log=trace --skynetapikey=I8JMMBPUNMEHGCV056IG1V8OEEIB0CJUTG97S0J3IQNJJ4CSGH0G --skynetapiurl='https://web3portal.com' --testnet)
