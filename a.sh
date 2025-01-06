b59f03d46fb9d4e109d03e2c95c6f325bcb8fc84
norwnd <norwnd>
2025-01-06 11:13:30 +0200

    instrument a.sh with pprof everywhere

--- a.sh
+++ a.sh
@@ -8,9 +8,9 @@ set -e
 (./client/cmd/bisonw/bisonw --webaddr=127.0.0.1:3333 --log=trace --httpprof)
 
 ## mainnet with dedicated DB:
-#(./client/cmd/bisonw/bisonw --db=/Users/norwnd/d-e-x-c-db/db_mainnet --webaddr=127.0.0.1:3333 --log=trace)
+#(./client/cmd/bisonw/bisonw --db=/Users/norwnd/d-e-x-c-db/db_mainnet --webaddr=127.0.0.1:3333 --log=trace --httpprof)
 ## testnet
-#(./client/cmd/bisonw/bisonw --db=/Users/norwnd/d-e-x-c-db/db_mainnet --webaddr=127.0.0.1:3333 --log=trace --testnet)
+#(./client/cmd/bisonw/bisonw --db=/Users/norwnd/d-e-x-c-db/db_mainnet --webaddr=127.0.0.1:3333 --log=trace --testnet --httpprof)
 
 ## mainnet with disaster backups:
-#(./client/cmd/bisonw/bisonw --webaddr=127.0.0.1:3333 --log=trace --skynetapikey=I8JMMBPUNMEHGCV056IG1V8OEEIB0CJUTG97S0J3IQNJJ4CSGH0G --skynetapiurl='https://web3portal.com' --testnet)
+#(./client/cmd/bisonw/bisonw --webaddr=127.0.0.1:3333 --log=trace --skynetapikey=I8JMMBPUNMEHGCV056IG1V8OEEIB0CJUTG97S0J3IQNJJ4CSGH0G --skynetapiurl='https://web3portal.com' --testnet --httpprof)
